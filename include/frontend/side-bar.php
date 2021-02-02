59df3caf543727c2885833cd5e78fca484042da4
THE IRF <the.irf.1234@gmail.com>
2021-02-02 17:56:41 +0400

    Code Uploaded 02 Feb 2021

---
+++ include/frontend/side-bar.php
@@ -0,0 +1,20 @@
+<div class="sidebar">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+          <a href="javascript:void(0)" class="simple-text logo-mini">
+            TechRoar
+          </a>
+          <a href="javascript:void(0)" class="simple-text logo-normal">
+            Property
+          </a>
+        </div>
+        <ul class="nav">
+          <li class="active ">
+            <a href="index.php">
+              <i class="tim-icons icon-chart-pie-36"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include 'include/frontend/header.php'; ?>
+<?php include 'include/frontend/side-bar.php'; ?>
+<?php include 'include/frontend/top-nav.php'; ?>
+      <div class="content">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card card-chart">
+              <div class="card-header">
+                <h5 class="card-category">Total Shipments</h5>
+                <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary"></i> 763,215</h3>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card card-chart">
+              <div class="card-header">
+                <h5 class="card-category">Daily Sales</h5>
+                <h3 class="card-title"><i class="tim-icons icon-delivery-fast text-info"></i> 3,500€</h3>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card card-chart">
+              <div class="card-header">
+                <h5 class="card-category">Completed Tasks</h5>
+                <h3 class="card-title"><i class="tim-icons icon-send text-success"></i> 12,100K</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+<?php include 'include/frontend/footer.php'; ?>
